590605a048de93b2e0e9ad925bd2473e6187fc72
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2019-06-23 14:33:23 -0400

    parsing json exports

--- index.js
+++ index.js
@@ -4,59 +4,70 @@ const fs = require("fs");
 const csv = require("csvtojson");
 const moment = require("moment");
 
-csv().fromFile(path.resolve(__dirname, "results.csv"))
-    .then((jsonObj)=>{
-        const list = jsonObj.map((obj) => {
-            return {
-                number: obj["REGISTRATIONNUMBER"],
-                registrant: obj["REGISTRANTNAME"],
-                date: moment(obj["STAMPED/RECEIVEDDATE"], "DD-MMM-YYYY").valueOf(),
-                link: [{
-                    url: obj["DOCUMENT_URL"],
-                    text: obj["DOCUMENTTYPE"],
-                    dateFiled: moment(obj["STAMPED/RECEIVEDDATE"], "DD-MMM-YYYY").valueOf()
-                }]
-            };
-        }).reduce((accumulator, currentValue) => {
-            const matching = accumulator.findIndex((obj) => obj.number === currentValue.number && obj.registrant === currentValue.registrant); // Will return -1 if no match...
-            if(matching === -1){
-                accumulator.push(currentValue);
-                if(accumulator.date < currentValue.date){
-                    accumulator.date = currentValue.date; // Set date (aka most recent filing)
+const convertCSVAndUpload = () => {
+
+    csv().fromFile(path.resolve(__dirname, "results.csv"))
+        .then((jsonObj)=>{
+            const list = jsonObj.map((obj) => {
+                return {
+                    number: obj["REGISTRATIONNUMBER"],
+                    registrant: obj["REGISTRANTNAME"],
+                    date: moment(obj["STAMPED/RECEIVEDDATE"], "DD-MMM-YYYY").valueOf(),
+                    link: [{
+                        url: obj["DOCUMENT_URL"],
+                        text: obj["DOCUMENTTYPE"],
+                        dateFiled: moment(obj["STAMPED/RECEIVEDDATE"], "DD-MMM-YYYY").valueOf()
+                    }]
                 };
-                return accumulator;
-            } else {
-                let oldLinks = accumulator[matching].link;
-                let newLink = currentValue.link;
-                accumulator[matching].link = [...oldLinks, ...newLink ];
-                return accumulator;
-            }
-        }, []).map((obj) => {
-            obj['allLinks'] = obj['link'].filter((link) => link.url !== "http://www.fara.gov/contact.html");
-            delete obj['link'];
+            }).reduce((accumulator, currentValue) => {
+                const matching = accumulator.findIndex((obj) => obj.number === currentValue.number && obj.registrant === currentValue.registrant); // Will return -1 if no match...
+                if(matching === -1){
+                    accumulator.push(currentValue);
+                    if(accumulator.date < currentValue.date){
+                        accumulator.date = currentValue.date; // Set date (aka most recent filing)
+                    };
+                    return accumulator;
+                } else {
+                    let oldLinks = accumulator[matching].link;
+                    let newLink = currentValue.link;
+                    accumulator[matching].link = [...oldLinks, ...newLink ];
+                    return accumulator;
+                }
+            }, []).map((obj) => {
+                obj['allLinks'] = obj['link'].filter((link) => link.url !== "http://www.fara.gov/contact.html");
+                delete obj['link'];
+                return obj;
+            });
+
+            const data = JSON.stringify(list);
+
+            fs.writeFile('fara.json', data, (err) => {  
+                if (err) throw err;
+                console.log('Data written to file');
+            });
+            
+        });
+};
+
+const removeExtraneous = async () => {  
+    let trimmed;
+
+    fs.readFile('senators.json', (err, data) => {  
+        if (err) throw err;
+        let content = JSON.parse(data);
+        trimmed = content.map((obj) => {
+            delete obj['_id'];
+            delete obj["__v"];
+            obj.date = parseInt(obj.date.$numberLong);
             return obj;
         });
 
-        const data = JSON.stringify(list);
-
-        fs.writeFile('fara.json', data, (err) => {  
+        fs.writeFile("senatorsTrimmed.json", JSON.stringify(trimmed), (err) => {
             if (err) throw err;
             console.log('Data written to file');
         });
-        
     });
+};
 
-
-
-    // reduce((accumulator, currentValue) => { // Combine identical items" links into single allLink
-    //         const matching = accumulator.findIndex((obj) => obj.number === currentValue.number && obj.registrant === currentValue.registrant); // Will return -1 if no match...
-    //         if(matching === -1){
-    //             accumulator.push(currentValue);
-    //             return accumulator;
-    //         } else {
-    //             let oldLinks = accumulator[matching].allLinks;
-    //             let newLink = currentValue.allLinks;
-    //             accumulator[matching].allLinks = [ ...oldLinks, ...newLink ];
-    //             return accumulator;
-    //         };
-    //     }, []);
+removeExtraneous();
+convertCSVAndUpload();
